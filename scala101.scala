42c9bc18e54110ea36d2637439c3755cb02f02db
Alban Phelip <albanphelip@MacBook-Pro-de-Alban-2.local>
2015-06-05 10:17:59 +0200

    scala101

--- scala101.scala
+++ scala101.scala
@@ -42,8 +42,8 @@ linesWithSpark.count()
 // Monter les données en mémoire
 myData.cache()
 
-/*
+/*****
 Pour aller plus loin : 
 https://github.com/xbucchiotty/scala-for-beginners
 https://github.com/xbucchiotty/codeur_en_seine 
-*/
+*****/
